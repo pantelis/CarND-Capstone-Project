d265e41cc82299c98aa87d13a2c5e9e94acf9fcd
Kinshuk <kinshuk.ram@gmail.com>
2018-02-17 15:00:24 +0100

    Add the computer vision based traffic light classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,8 +1,22 @@
 from styx_msgs.msg import TrafficLight
 
+import tensorflow as tf
+from keras.models import load_model
+from keras.utils.data_utils import get_file
+import os
+import numpy as np
+import cv2
+from cv_classifier import estimate_label
+
 class TLClassifier(object):
-    def __init__(self):
-        #TODO load classifier
+    def __init__(self, model_path=None):
+        # DONE load classifier
+        if model_path:
+            self.model_path = model_path
+            self.is_cv_based = False # Use ML based model
+        else:
+            self.is_cv_based = True # Use Computer Vision based classification
+
         pass
 
     def get_classification(self, image):
@@ -16,4 +30,6 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+        if self.is_cv_based:
+            return estimate_label(image)
         return TrafficLight.UNKNOWN
